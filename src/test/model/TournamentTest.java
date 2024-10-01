38e3a457ad0ab316d399215933dce3be7534e108
yu38 <yu38@students.cs.ubc.ca>
2024-10-01 00:05:12 -0700

    Added tests for Tournament class

---
+++ src/test/model/TournamentTest.java
@@ -0,0 +1,57 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TournamentTest {
+    private Tournament testTournament;
+    private Player p1;
+    private Player p2;
+
+    @BeforeEach
+    void runBefore() {
+        testTournament = new Tournament();
+        p1 = new Player("Rafael Nadal");
+        p2 = new Player("Andy Murray");
+    }
+
+    @Test
+    void testConstructor() {
+        ArrayList<Player> players = testTournament.getPlayers();
+        assertEquals(0, players.size());
+    }
+
+    @Test
+    void testAddPlayer() {
+        ArrayList<Player> players = testTournament.getPlayers();
+        testTournament.addPlayer(p1);
+        assertEquals(1, players.size());
+        assertEquals(p1, players.get(0));
+    }
+
+    @Test
+    void testAddPlayerMultiple() {
+        ArrayList<Player> players = testTournament.getPlayers();
+        testTournament.addPlayer(p1);
+        assertEquals(1, players.size());
+        assertEquals(p1, players.get(0));
+        testTournament.addPlayer(p2);
+        assertEquals(2, players.size());
+        assertEquals(p2, players.get(1));
+    }
+
+    @Test
+    void testAddPlayerSamePlayer() {
+        ArrayList<Player> players = testTournament.getPlayers();
+        testTournament.addPlayer(p1);
+        assertEquals(1, players.size());
+        assertEquals(p1, players.get(0));
+        testTournament.addPlayer(p1);
+        assertEquals(1, players.size());
+        assertEquals(p1, players.get(0));
+    }
+}
