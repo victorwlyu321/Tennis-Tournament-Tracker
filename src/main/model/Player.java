d0a7a57bfb6fa4393202216cab258b9599f5ad0f
yu38 <yu38@students.cs.ubc.ca>
2024-09-30 23:27:23 -0700

    Added implementation for Player class

--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -3,38 +3,43 @@ package model;
 // Represents a tennis player having a name, number of match wins, 
 // and number of match losses
 public class Player {
+    private String name;
+    private int wins;
+    private int losses;
 
     // REQUIRES: name has a non-zero length
     // EFFECTS: create a tennis player with the given name,
     //          zero match wins, and zero match losses
     public Player(String name) {
-        // stub
+        this.name = name;
+        this.wins = 0;
+        this.losses = 0;
     }
 
     // MODIFIES: this
     // EFFECTS: increment player's number of match wins by 1
     public void increaseMatchWin() {
-        // stub
+        this.wins += 1;
     }
 
     // MODIFIES: this
     // EFFECTS: increment player's number of match losses by 1
     public void increaseMatchLoss() {
-        // stub
+        this.losses += 1;
     }
 
     // getter
     public String getName() {
-        return null;
+        return this.name;
     }
 
     // getter
     public int getMatchWins() {
-        return 0;
+        return this.wins;
     }
 
     // getter
     public int getMatchLosses() {
-        return 0;
+        return this.losses;
     }
 }
