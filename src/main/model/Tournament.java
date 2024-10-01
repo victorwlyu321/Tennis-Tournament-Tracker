93ac7d812b0ac67dae2ef40526ca928fd934051a
yu38 <yu38@students.cs.ubc.ca>
2024-10-01 00:09:11 -0700

    Implemented Tournament class

--- src/main/model/Tournament.java
+++ src/main/model/Tournament.java
@@ -4,20 +4,23 @@ import java.util.ArrayList;
 
 // Represents a tennis tournament having a list of participating tennis players
 public class Tournament {
+    private ArrayList<Player> players;
 
     // EFFECTS: create a tournament with an empty list of tennis players
     public Tournament() {
-        // stub
+        this.players = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds a tennis player to the list of players in the tournament
     public void addPlayer(Player player) {
-        // stub
+        if (!this.players.contains(player)) {
+            this.players.add(player);
+        }
     }
 
     // getter
     public ArrayList<Player> getPlayers() {
-        return new ArrayList<>();
+        return this.players;
     }
 }
