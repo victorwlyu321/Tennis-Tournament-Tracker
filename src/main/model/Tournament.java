12adaeaa5ee31b4b35b12b2f2a37ee06bfb59a89
yu38 <yu38@students.cs.ubc.ca>
2024-09-30 23:44:57 -0700

    Added specification for Tournament class

---
+++ src/main/model/Tournament.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
+// Represents a tennis tournament having a list of participating tennis players
+public class Tournament {
+
+    // EFFECTS: create a tournament with an empty list of tennis players
+    public Tournament() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a tennis player to the list of players in the tournament
+    public void addPlayer(Player player) {
+        // stub
+    }
+
+    // getter
+    public ArrayList<Player> getPlayers() {
+        return null;
+    }
+}
