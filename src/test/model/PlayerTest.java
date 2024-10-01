e35884a5e6324b6956fcdc2458f617e1ea698ef6
yu38 <yu38@students.cs.ubc.ca>
2024-09-30 23:22:41 -0700

    Added tests for Player class

---
+++ src/test/model/PlayerTest.java
@@ -0,0 +1,64 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    private Player testPlayer;
+
+    @BeforeEach
+    void runBefore() {
+        testPlayer = new Player("Roger Federer");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Roger Federer", testPlayer.getName());
+        assertEquals(0, testPlayer.getMatchWins());
+        assertEquals("Roger Federer", testPlayer.getMatchLosses());
+    }
+
+    @Test
+    void testIncreaseMatchWin() {
+        assertEquals(0, testPlayer.getMatchWins());
+        testPlayer.increaseMatchWin();
+        assertEquals(1, testPlayer.getMatchWins());
+        assertEquals(0, testPlayer.getMatchLosses());
+    }
+
+    @Test
+    void testIncreaseMatchWinMultiple() {
+        assertEquals(0, testPlayer.getMatchWins());
+        testPlayer.increaseMatchWin();
+        assertEquals(1, testPlayer.getMatchWins());
+        assertEquals(0, testPlayer.getMatchLosses());
+        testPlayer.increaseMatchWin();
+        assertEquals(2, testPlayer.getMatchWins());
+        testPlayer.increaseMatchWin();
+        assertEquals(3, testPlayer.getMatchWins());
+        assertEquals(0, testPlayer.getMatchLosses());
+    }
+
+    @Test
+    void testIncreaseMatchLoss() {
+        assertEquals(0, testPlayer.getMatchLosses());
+        testPlayer.increaseMatchLoss();
+        assertEquals(1, testPlayer.getMatchLosses());
+        assertEquals(0, testPlayer.getMatchWins());
+    }
+
+    @Test
+    void testIncreaseMatchLossMultiple() {
+        assertEquals(0, testPlayer.getMatchLosses());
+        testPlayer.increaseMatchLoss();
+        assertEquals(1, testPlayer.getMatchLosses());
+        assertEquals(0, testPlayer.getMatchWins());
+        testPlayer.increaseMatchLoss();
+        assertEquals(2, testPlayer.getMatchLosses());
+        testPlayer.increaseMatchLoss();
+        assertEquals(3, testPlayer.getMatchLosses());
+        assertEquals(0, testPlayer.getMatchWins());
+    }
+}
