c2022e28f40cae64f17e40b23c84f37f42a30b80
yu38 <yu38@students.cs.ubc.ca>
2024-10-02 00:34:28 -0700

    Added specification for TennisTournamentApp class

---
+++ src/main/ui/TennisTournamentApp.java
@@ -0,0 +1,68 @@
+package ui;
+
+import model.Player;
+
+// A Tennis Tournament Application that allows users to add tennis players to a tournament, 
+// view the list of players, view players' win-loss records, and specify the winner and loser of a match
+public class TennisTournamentApp {
+
+    // EFFECTS: runs the tennis tournament application
+    public TennisTournamentApp() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes tennis tournament with starting values
+    public void init() {
+        // stub
+    }
+
+    // EFFECTS: processes user inputs from the main menu
+    private void handleMenu() {
+        // stub
+    }
+
+    // EFFECTS: displays menu options to user
+    private void displayMenu() {
+        // stub
+    }
+
+    // EFFECTS: processes user's input
+    private void processCommand(String input) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates a new tennis player and 
+    //          adds the player to the list of players in the tournament
+    private void addNewPlayer() {
+        // stub
+    }
+
+    // EFFECTS: prompts user to select a tennis player from list of players
+    private Player selectPlayer(String player) {
+        return null;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: selects the winner of a match and increases the number of match wins of the player
+    private void specifyWinner(String winner) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: selects the loser of a match and increases the number of match losses of the player
+    private void specifyLoser(String loser) {
+        // stub
+    }
+
+    // EFFECTS: returns a list of players in the tournament
+    private String displayPlayers() {
+        return "";
+    }
+
+    // EFFECTS: returns a player's win-loss record
+    private String displayPlayerRecord() {
+        return "";
+    }
+}
