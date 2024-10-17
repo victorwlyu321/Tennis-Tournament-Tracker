a551bafd09e285d9c90bf83f4aea59e129a594ac
yu38 <yu38@students.cs.ubc.ca>
2024-10-17 16:06:11 -0700

    Added tests for JsonReaderTest class, added JsonReader test JSON files

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,57 @@
+package persistence;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import model.Player;
+import model.Tournament;
+
+// Referenced JsonReaderTest in JsonSerializationDemo
+
+public class JsonReaderTest extends JsonTest {
+
+    @Test
+    public void testReaderFileDoesNotExist() {
+        JsonReader reader = new JsonReader("./data/fileDoesNotExist.json");
+
+        try {
+            Tournament tn = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // does nothing, pass
+        }
+    }
+
+    @Test
+    public void testReaderEmptyTournament() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyTournament.json");
+
+        try {
+            Tournament tn = reader.read();
+            assertEquals(0, tn.getPlayers().size());
+        } catch (IOException e) {
+            fail("Error reading file");
+        }
+    }
+
+    @Test
+    public void testReaderRegularTournament() {
+        JsonReader reader = new JsonReader("./data/testReaderRegularTournament.json");
+
+        try {
+            Tournament tn = reader.read();
+            ArrayList<Player> players = tn.getPlayers();
+            assertEquals(3, players.size());
+            checkPlayer("Roger Federer", 5, 2, players.get(0));
+            checkPlayer("Rafael Nadal", 4, 3, players.get(1));
+            checkPlayer("Novak Djokovic", 2, 5, players.get(2));
+        } catch (IOException e) {
+            fail("Error reading file");
+        }
+    }
+}
