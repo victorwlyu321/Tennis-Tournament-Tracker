103f0172b310d48439f12039cea5073edd2b788d
yu38 <yu38@students.cs.ubc.ca>
2024-10-17 22:27:25 -0700

    Removed unnecessary code in JsonReaderTest class

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,8 +1,6 @@
 package persistence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
