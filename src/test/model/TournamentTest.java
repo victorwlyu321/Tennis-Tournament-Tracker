a93102776927db54a0da9d1ac8dd49f4b5c22fda
yu38 <yu38@students.cs.ubc.ca>
2024-10-17 22:21:46 -0700

    Added tests for JSON related methods for TournamentTest class

--- src/test/model/TournamentTest.java
+++ src/test/model/TournamentTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -61,8 +63,40 @@ public class TournamentTest {
     }
 
     @Test
-    void tesetFindPlayerDoesNotExist() {
+    void testFindPlayerDoesNotExist() {
         testTournament.addPlayer("Rafael Nadal");
         assertNull(testTournament.findPlayer("Roger Federer"));
+    }
+
+    @Test
+    void testToJson() {
+        assertTrue(testTournament.addPlayer("Rafael Nadal"));
+        assertTrue(testTournament.addPlayer("Andy Murray"));
+        ArrayList<Player> players = testTournament.getPlayers();
+
+        JSONObject testJsonObject = testTournament.toJson();
+        JSONArray testJsonArray = testJsonObject.getJSONArray("players");
+
+        assertEquals(players.get(0).getName(), testJsonArray.getJSONObject(0).getString("name"));
+        assertEquals(players.get(0).getMatchWins(), testJsonArray.getJSONObject(0).getInt("wins"));
+        assertEquals(players.get(0).getMatchLosses(), testJsonArray.getJSONObject(0).getInt("losses"));
+        assertEquals(players.get(1).getName(), testJsonArray.getJSONObject(1).getString("name"));
+        assertEquals(players.get(1).getMatchWins(), testJsonArray.getJSONObject(1).getInt("wins"));
+        assertEquals(players.get(1).getMatchLosses(), testJsonArray.getJSONObject(1).getInt("losses"));
+    }
+
+    @Test
+    void testPlayersToJson() {   
+        assertTrue(testTournament.addPlayer("Rafael Nadal"));
+        assertTrue(testTournament.addPlayer("Andy Murray"));
+        ArrayList<Player> players = testTournament.getPlayers();
+
+        JSONArray testJsonArray = testTournament.playersToJson();
+        assertEquals(players.get(0).getName(), testJsonArray.getJSONObject(0).getString("name"));
+        assertEquals(players.get(0).getMatchWins(), testJsonArray.getJSONObject(0).getInt("wins"));
+        assertEquals(players.get(0).getMatchLosses(), testJsonArray.getJSONObject(0).getInt("losses"));
+        assertEquals(players.get(1).getName(), testJsonArray.getJSONObject(1).getString("name"));
+        assertEquals(players.get(1).getMatchWins(), testJsonArray.getJSONObject(1).getInt("wins"));
+        assertEquals(players.get(1).getMatchLosses(), testJsonArray.getJSONObject(1).getInt("losses"));
     }
 }
